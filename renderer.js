356f69348a831295abd6598f403938ea5410d191
Shageki <seanmburgess@gmail.com>
2018-10-24 12:29:16 -0500

    Initial commit

---
+++ renderer.js
@@ -0,0 +1,5 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+// TODO: This should contain any top-level controllers like keyboard mapping I think?
+// TODO: Read https://medium.com/cameron-nokes/deep-dive-into-electrons-main-and-renderer-processes-7a9599d5c9e2
